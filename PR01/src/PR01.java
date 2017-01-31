ffe840b7a2bd29e4dd147c8422410c132bde5bae
Stiv Kapten <stkapt@gmail.com>
2017-01-31 11:45:45 +0200

    esimene commit

---
+++ PR01/src/PR01.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Stiv on 31/01/2017.
+ */
+public class PR01 {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    public static String printAndReturnHello(String name) {
+        if (name == null) {
+            return null;
+        }
+        else {
+
+        }
+
+    }
+}
