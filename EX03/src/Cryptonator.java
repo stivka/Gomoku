7e985ba1bbb279dd6e3744b300055296152e4ef2
Stiv Kapten <stkapt@gmail.com>
2017-02-20 20:24:17 +0200

    also done find most in frequent

--- EX03/src/Cryptonator.java
+++ EX03/src/Cryptonator.java
@@ -8,6 +8,7 @@ public class Cryptonator {
     public static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     public static String stringAlphabet = "abcdefghijklmnopqrstuvwxyz";
     public static final int NUM_LETTERS_ALPHABET = 26;
+//    public static int[] frequency = new int[NUM_LETTERS_ALPHABET];
 
     /**
      * Given text and a rotation, encrypts text.
@@ -50,7 +51,8 @@ public class Cryptonator {
                 encryption += plainText.charAt(c);
             }
         }
-        System.out.println("\n" + encryption);
+        System.out.println("\n" + encryption + " (is what i got)");
+        findMostInfrequentlyOccurringLetter(encryption);
         return encryption;
     }
 
@@ -61,8 +63,82 @@ public class Cryptonator {
      * If there is no such letter, return 0.
      */
     public static char findMostInfrequentlyOccurringLetter(String text) {
-        // TODO: implement
-        return 0;
+        int[] frequency = new int[NUM_LETTERS_ALPHABET];
+        char mostInfrequentChar = '!';
+        boolean printed = false;
+        int minCount = text.length() + 1; /* count of the smallest value that a letter occurs so far. Set at maximum
+        value, which is that all characters of that string are that letter, plus one, so that .*/
+        System.out.println("Down below are shown the occurrence counts of letters.");
+        // This cycle counts every occurrence of every letter.
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                frequency[stringAlphabet.indexOf(text.charAt(i))] += 1;
+            }
+        }
+        // This cycle prints how many counts there are per letter's position in the alphabet.
+        for (int j = 0; j < frequency.length; j++) {
+            System.out.print(frequency[j] + " ");
+        }
+        /* In this loop it is checked whether there is one letter that occurs more than others. This is necessary
+           to determine that there even is a most infrequently occurring letter, because if all letters occur only once
+           like for example "Love hurts" - then there can't be a most infrequent letter. But if the message was "Love
+           doesn't hurt" - then we already get letters like o, e and t, that occur more than once, leaving us with the
+           distinction that there are letters, which on the other hand, occur only once.*/
+        for (int k = 0; k < frequency.length; k++) {
+            if (frequency[k] > 0 && frequency[k] <= minCount) {
+                minCount = frequency[k];
+                break;
+            } else if (frequency[k] < minCount && k == frequency.length - 1) {
+                /* If one letter has occurred at least once, but less times than any other letter from the whole
+                 alphabet that has occurred at least once,If one letter has not occurred more than once from the whole alphabet.
+
+                */
+                System.out.println("\nAll chars occur only once. There isn't a least frequent char.");
+                return 0;
+            }
+
+        }
+        // This cycle returns the first character in alphabetical order, that occurs the least.
+        for (int l = 0; l < frequency.length; l++) {
+            if (frequency[l] == minCount) {
+                mostInfrequentChar = stringAlphabet.charAt(l);
+                break;
+            }
+        }
+        System.out.println("\nMost infrequent char in the order of the alphabet is: " + mostInfrequentChar);
+        return mostInfrequentChar;
+    }
+
+    /**
+     * By mistake created this method for finding the most FREQUENTLY, not infrequently, occurring character.
+     * @param text is the encrypted string.
+     * @return the most frequently occurring character or 0 if all characters occur only once.
+     */
+    public static char findMostFrequentlyOccurringLetter(String text) {
+        int[] frequency = new int[NUM_LETTERS_ALPHABET];
+        int mostFrequentCharCount = -1;
+        char mostFrequentChar = '!';
+        System.out.println("Down below are shown the occurrence counts of letters.");
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                frequency[stringAlphabet.indexOf(text.charAt(i))] += 1;
+            }
+        }
+        for (int j = 0; j < frequency.length; j++) {
+            System.out.print(frequency[j] + " ");
+        }
+        for (int k = 0; k < frequency.length; k++) {
+            if (frequency[k] > mostFrequentCharCount) {
+                mostFrequentCharCount = frequency[k];
+                mostFrequentChar = stringAlphabet.charAt(k);
+            }
+        }
+        if (mostFrequentChar == 1) {
+            return 0;
+        } else {
+            System.out.println("\n" + mostFrequentChar);
+            return mostFrequentChar;
+        }
     }
 
     /**
@@ -138,10 +214,20 @@ public class Cryptonator {
          Peale krüpteerimist esineb tähti "i", "l", "b", "c" kõik üks kord. "b" on tähestikus kõige eespool,
          seepärast eemaldatakse tulemusest "b" (algselt "r" täht).
          */
-        System.out.println("you too Brutus?" + " (is the plain text - not encrypted message)");
-        System.out.println("iye dyy ledec?" + " (is the right outcome)");
-        System.out.println("xnt snn aqtstr?" + " (for rotation 1");
+
+        System.out.println("\"you too Brutus?\" (is the plain text - not encrypted message)\n" +
+                "\"iye dyy ledec?\" (is the right outcome with a rotation 16)");
         encrypt("you too Brutus?", 16);
+
+        System.out.println("\n\"you too Brutus?\" on a rotation 1, should give \n\"xnt snn aqtstr?\"");
         encrypt("you too Brutus?", 1);
+
+        System.out.println("\nExample when all letters occur only once. And on a high rotation \"Love Hurts\", " +
+                "on a rotation of 40 000");
+        encrypt("Love Hurts", 40000);
+
+        System.out.println("\nAnother example when ALL letters occur TWICE! And only twice! Also testing a negative" +
+                " rotation. \"Couscous\", on a rotation of -1");
+        encrypt("Couscous", -1);
     }
 }
