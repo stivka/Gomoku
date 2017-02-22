390a9864f88634edea318c3ebab0e3dc58bd4855
Stiv Kapten <stkapt@ttu.ee>
2017-02-22 17:34:49 +0200

    just understanding the solving part

--- PR05/src/Quadratic.java
+++ PR05/src/Quadratic.java
@@ -19,7 +19,7 @@ public class Quadratic {
      */
     public static double[] solve(double a, double b, double c) {
         // TODO: a == 0
-        double d = b * b - 4 * a * c;
+        double d = b * b - 4 * a * c; // This is the part under the square root.
         if (d > 0) {
             return new double[]{(-b + Math.sqrt(d)) / 2 / a, (-b - Math.sqrt(d)) / 2 / a};
         } else if (d == 0) {
@@ -30,6 +30,7 @@ public class Quadratic {
     }
 
     public static void main(String[] args) {
+        System.out.println("-x^2 + 2x + 3 = 0");
         System.out.println(Arrays.toString(solve(-1, 2, 3)));
     }
 }
