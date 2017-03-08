c32ab9ba2aeb0dc85409e80f5f362188a1d9ffa8
Stiv Kapten <stkapt@gmail.com>
2017-03-08 15:51:01 +0200

    j'rgmine

--- EX05/src/ee/ttu/java/albumcreation/Album.java
+++ EX05/src/ee/ttu/java/albumcreation/Album.java
@@ -1,16 +1,35 @@
 package ee.ttu.java.albumcreation;
 
 /**
- * Created by Stiv on 07/03/2017.
+ * Created by Stiv on 08/03/2017.
  */
-public Album() //Klassi konstruktor
-        String title
-        List trackListing //list, kus hoitakse Song tüüpi objekte
-        String genre
-public void addSong(Song)
-public void removeSong(Song)
-/**
- * Albumi saab avaldada alles siis, kui seal on vähemalt 5 laulu ja kestab kauem vähemalt 25 minutit.
- * Albumil peab olema määratud ka nimi ja žanr. Kõik albumil olevad lood peavad olema valmis.
- */
-public boolean release()
+public class Album { //Klassi konstruktor
+    /**
+     *
+     */
+    private String title;
+//    List trackListing //list, kus hoitakse Song tüüpi objekte
+    /**
+     *
+     */
+    private String genre;
+
+    /**
+     *
+     */
+    public void addSong(Song) {
+    }
+
+    public void removeSong(Song) {
+    }
+
+    public boolean release() {
+    } //Albumi saab avaldada alles siis, kui seal on vähemalt 5 laulu ja kestab kauem vähemalt 25 minutit. Albumil peab olema määratud ka nimi ja žanr. Kõik albumil olevad lood peavad olema valmis.
+
+    public void setTitle(String album1) {
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
--- EX05/src/ee/ttu/java/albumcreation/Song.java
+++ EX05/src/ee/ttu/java/albumcreation/Song.java
@@ -1,12 +1,69 @@
 package ee.ttu.java.albumcreation;
 
+import java.util.*;
+
 /**
- * Created by Stiv on 07/03/2017.
+ * Created by Stiv on 08/03/2017.
  */
-public Song() //Klassi konstruktor
-        String title
-        List authors //List, kus hoitakse String tüüpi andmeid
-        String length
-public void addAuthor(String)
-public void removeAuthor(String)
-public boolean isReady() //Laulul peavad olema määratud pealkiri, vähemalt üks autor ning see peab olema aktsepteeritava pikkusega
+public class Song { //Klassi konstruktor
+    /**
+     *
+     */
+    private String title;
+    /**
+     *
+     */
+    private List<String> authors = new ArrayList<String>(); /* right now by
+    default the capacity of the list is 10 - cannot fit more
+    authors. */
+    /**
+     *
+     */
+    private String length;
+
+    public void isReady() {
+    }
+
+    public void setTitle(String s) {
+        this.title = s;
+    }
+
+    /**
+     * meetod aktsepteerib loo pikkust järgmistel kujudel:
+     * mm:ss, mm:s, m:ss, m:s (m - minut, s - sekund). Ehk siis
+     * sobivad on "01:02", "01:2", "1:02", "1:2". Kõik annavad sama
+     * tulemuse. Kui sisendina saadud sõne pole korrektne (ei vasta
+     * eelnevatele formaatidele või sisaldab muid sümboleid), ei
+     * muudeta olemasolevat loo pikkust. Algselt on loo pikkus null.
+     * @param s sth like "02:43"
+     */
+    public void setLength(String s) {
+        int colonCount = 0;
+        int otherThanDigitOrColon = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ':') {
+                colonCount++;
+            }
+            else if (!Character.isDigit(s.charAt(i))) {
+                otherThanDigitOrColon++;
+            }
+        }
+        /* if there's only one colon and nothing other tha digits*/
+        if (colonCount == 1 && otherThanDigitOrColon == 0) {
+
+        }
+        if (s.length() == 3 && s.charAt(1) == ':') {
+
+        }
+        this.length = s;
+    }
+
+    public void addAuthor(String me) {
+    }
+
+    public void removeAuthor(String s) {
+    }
+/* Laulul peavad olema määratud pealkiri, vähemalt üks autor ning see
+peab olema aktsepteeritava pikkusega */
+
+}
