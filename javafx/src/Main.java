6f5bdc856bdec563218b55edb9bbfac436cdd24c
Stiv Kapten <stkapt@gmail.com>
2017-03-27 15:18:44 +0300

    javafx

---
+++ javafx/src/Main.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Stiv on 27/03/2017.
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+/**
+ *
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        BorderPane main = new BorderPane();
+        Scene scene = new Scene(main);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setHeight(600);
+        primaryStage.setWidth(800);
+
+        javafx.scene.control.Button button = new Button("Press here!");
+
+        button.setOnAction(event -> {
+            System.out.println("tere:" + ((Button)event.getSource()).getText());
+        });
+
+
+        main.setCenter(button);
+
+
+    }
+}
