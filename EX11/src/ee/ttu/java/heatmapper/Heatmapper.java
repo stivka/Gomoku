365ec231fa92aa82b42455670144f6ee0e233281
Stiv Kapten <stkapt@gmail.com>
2017-04-18 12:35:47 +0300

    Heatmapper is not public in ee.ttu.java...?

--- EX11/src/ee/ttu/java/heatmapper/Heatmapper.java
+++ EX11/src/ee/ttu/java/heatmapper/Heatmapper.java
@@ -1,7 +1,6 @@
 package ee.ttu.java.heatmapper;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
